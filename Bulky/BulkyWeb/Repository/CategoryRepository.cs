554b10f5374663ed9a784ff6f9636e2c134eb03e
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2025-08-04 08:07:31 +0530

    till now before using repository pattern in categoryController, crud operation
    Implemented Category CRUD operations using Entity Framework Core, added repository and unit of work patterns, and created corresponding views for Create, Edit, Delete, and Index. Updated layout and notification partial for user feedback. Added initial database migrations and seeded sample data. Configured SQL Server connection and registered ApplicationDbContext in DI container.
    
    After this commit i use repository pattern in category controller

---
+++ Bulky/BulkyWeb/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using BulkyWeb.Repository.IRepository;
+
+namespace BulkyWeb.Repository
+{
+    public class CategoryRepository :  Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save(Category category)
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Update(category);
+        }
+    }
+}
